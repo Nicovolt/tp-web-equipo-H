65fb08a63ae0b4df4aaabbf4820a48f1f02a4a38
Nicovolt <166955943+Nicovolt@users.noreply.github.com>
2024-05-09 17:12:25 -0300

    Agregado de busqueda
    agregue un boton y un text box para buscar articulos (sin funcionalidad)
    y cambie la forma en la que se ven los articulos.

--- TPWinForm-equipo-h/TPWinForm-equipo-h/Main.Master.designer.cs
+++ TPWinForm-equipo-h/TPWinForm-equipo-h/Main.Master.designer.cs
@@ -33,13 +33,13 @@ namespace TPWinForm_equipo_h
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Control tamCarrito.
+        /// Control Label1.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label tamCarrito;
+        protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
         /// Control ContentPlaceHolder1.
